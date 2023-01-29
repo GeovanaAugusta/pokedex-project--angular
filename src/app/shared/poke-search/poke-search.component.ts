b4410ab035a9f7f58b0d27ca7458e5849b303566
Geovana Augusta <geovanaag@hotmail.com>
2023-01-28 23:03:40 -0300

    feat: implementa o emit para recuperar o que for digitado na barra de busca

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
   templateUrl: './poke-search.component.html',
   styleUrls: ['./poke-search.component.scss']
 })
-export class PokeSearchComponent {
+export class PokeSearchComponent implements OnInit {
+
+  // O que for digitado deve ser emitido pra outros componentes, facilitando a buca
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  public search(search: string) {
+    this.emmitSearch.emit(search);
+    console.log(search);
+
+  }
 
 }
