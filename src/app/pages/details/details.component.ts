8df95f72031fbcb520c8dfc6fecb1e255f0af2a4
Geovana Augusta <geovanaag@hotmail.com>
2023-01-29 00:16:20 -0300

    feat: finaliza estilização da página de detalhamento de Pokemons

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { forkJoin } from 'rxjs';
+import { combineLatest } from 'rxjs';
 import { PokeAPIService } from 'src/app/service/poke-api.service';
+import { PokemonDetailsI } from '../interface';
 
 @Component({
   selector: 'app-details',
@@ -13,24 +14,34 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: PokemonDetailsI[] = [];
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
   // Pegar o ID presente na URL
   constructor(private activatedRoute: ActivatedRoute,
     private pokeAPIService: PokeAPIService) {}
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon() {
+  public get getPokemon() {
     const id = this.activatedRoute.snapshot.params['id'];
     // Pegar as informações do Pokemon com o id acima
     const pokemon = this.pokeAPIService.apiGetPokemon(`${this.urlPokemon}/${id}`);
     const name = this.pokeAPIService.apiGetPokemon(`${this.urlName}/${id}`);
 
     // Faz duas buscas, dispensando o uso de subscribes
-   return forkJoin([pokemon, name]).subscribe(
+    // O forkJoin que usava a mesma estrutura abaixo, foi substituído pelo combineLatest
+   return combineLatest([pokemon, name]).subscribe(
       res => {
+        this.pokemon = res;
+        this.isLoading = true;
         console.log(res);
+      },
+      error => {
+        this.apiError = true;
       }
     );
   }
--- src/app/pages/interface.ts
+++ src/app/pages/interface.ts
@@ -1,4 +1,4 @@
-import { types } from "joi"
+import { any, string, types } from "joi"
 
 export interface PokemonI {
 
@@ -13,9 +13,31 @@ export interface PokemonResultI {
     count: number,
     next: string
     previous: null,
-    results: any[],
+    results: any,
     url: string,
     status: any,
     name: string,
 
+}
+
+export interface PokemonDetailsI
+  {
+    abilities: any,
+    base_experience: number,
+    forms: any,
+    game_indices: any,
+    height: number,
+    held_items: any,
+    id: number,
+    is_default: boolean,
+    location_area_encounters: string,
+    move: any,
+    name: string,
+    order: number,
+    past_types: any,
+    species: any,
+    sprites: any,
+    versions: any,
+    stats: any,
+    types: any,
 }
